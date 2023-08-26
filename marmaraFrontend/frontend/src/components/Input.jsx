7c5e4762956ed182e45d9c25aabb5d593ac3e0ae
serkan-bayram <serkanbay02@hotmail.com>
2023-08-26 11:51:08 +0300

    first request try

--- marmaraFrontend/frontend/src/components/Input.jsx
+++ marmaraFrontend/frontend/src/components/Input.jsx
@@ -6,10 +6,18 @@ function Input(props) {
     width: "100%",
   };
 
+  const handleOnChange = (e) => {
+    const value = e.target.value;
+    props.setState((prevValues) => {
+      return { ...prevValues, [props.about]: value };
+    });
+  };
+
   return (
     <div className="mt-12 w-full flex border-2 border-b-slate-300 border-x-0 border-t-0 p-2 pl-0 ">
       <img className="w-6 h-6" src={props.src} alt={props.alt} />
       <input
+        onChange={handleOnChange}
         className="pl-2 outline-0 border-0"
         style={props.type === "Date" ? styles : {}}
         type={props.type}
--- marmaraFrontend/frontend/src/components/RouterButton.jsx
+++ marmaraFrontend/frontend/src/components/RouterButton.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function RouterButton(props) {
-  const { to, text } = props;
+  const { to, text, onClickFunction } = props;
+
+  const handleClick = () => {
+    if (onClickFunction) {
+      onClickFunction();
+    }
+  };
 
   return (
     <Link
+      onClick={handleClick}
       to={to}
       role="button"
       className="w-32 py-2 bg-accent rounded-full text-white font-bold tracking-wide text-center"
