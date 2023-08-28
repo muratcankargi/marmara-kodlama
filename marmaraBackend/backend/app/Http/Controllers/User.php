928932f9f3ceb44feca1d5989eaebf1d71763ab3
muratcankargi <muratcankargi@hotmail.com>
2023-08-28 19:32:42 +0300

    Update User.php

--- marmaraBackend/backend/app/Http/Controllers/User.php
+++ marmaraBackend/backend/app/Http/Controllers/User.php
@@ -7,16 +7,46 @@ use Illuminate\Http\Request;
 class User extends Controller
 {
 
-    public function isStudent()
+    public function isStudent(Request $request)
     {
-        $data = $this->json();
+        $data = ($request->all());
+
+        $personalId = $data['TCKimlikNo'];
+        $fatherName = $data['BabaAdi'];
+        $birthDate = $data['DogumTarihi'];
+
+        $formatControl=preg_match('/^\d{4}-\d{2}-\d{2}$/', $birthDate);
+
+        if ($personalId != "" && $fatherName != "" && $formatControl) {
+
+            $response = $this->doCurl(json_encode($data),'POST');
+
+            if ($response->OgrenciNo) {
+
+                return ['name' => $response->Ad, 'surname' => $response->Soyad, 'studentNumber' => $response->OgrenciNo];
+
+            } else {
+
+                return "false";
+
+            }
+        }
+        else{
+            return "false";
+        }
+
+
+    }
+
+    public function doCurl($data,$request)
+    {
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => $_ENV['CURLOPT_URL'],
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => '{"TCKimlikNo":' . $data->TCKimlikNo . ',"BabaAdi":"' . $data->BabaAdi . '","DogumTarihi":"' . $data->DogumTarihi . '"}',
+            CURLOPT_CUSTOMREQUEST => $request,
+            CURLOPT_POSTFIELDS => $data,
             CURLOPT_HTTPHEADER => array(
                 'Accept: application/json, text/javascript, */*; q=0.01',
                 'Content-Type: application/json',
@@ -28,25 +58,7 @@ class User extends Controller
         $response = json_decode(curl_exec($curl));
 
         curl_close($curl);
-        if($response->OgrenciNo){
-            return ['name'=>$response->Ad, 'surname'=>$response->Soyad, 'studentNumber'=>$response->OgrenciNo];
-        }
-        else{
-            return false;
-        }
+
+        return $response;
     }
-
-    public static function json()
-    {
-
-        $input = file_get_contents("php://input");
-        $json = json_decode($input);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            return false;
-        }
-
-        return $json;
-    }
-
 }
