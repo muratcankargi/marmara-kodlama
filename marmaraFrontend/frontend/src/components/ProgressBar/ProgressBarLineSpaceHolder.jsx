c0479072cacddb278d2244feecb9338410495bd4
Serkan <Serkanbay02@hotmail.com>
2023-09-04 20:39:47 +0300

    Delete marmaraFrontend directory

--- marmaraFrontend/frontend/src/components/ProgressBar/ProgressBarLineSpaceHolder.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// Siyah çizgi, 2 tane var w-24'ü değiştirirsen Circle'lar arası width artar
-function ProgressBarLineSpaceHolder() {
-  return <div className="w-24 h-2 "></div>;
-}
-
-export default ProgressBarLineSpaceHolder;
