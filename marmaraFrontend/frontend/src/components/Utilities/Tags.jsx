c751e40b6a0a2cb8a56c0e1a7ef94eda03a20a40
serkan-bayram <serkanbay02@hotmail.com>
2023-09-25 22:46:51 +0300

    change tags design

--- marmaraFrontend/frontend/src/components/Utilities/Tags.jsx
+++ marmaraFrontend/frontend/src/components/Utilities/Tags.jsx
@@ -1,43 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-function TagsHeader({ showMoreTags, setShowMoreTags }) {
-  const handleClick = () => {
-    setShowMoreTags(false);
-  };
-
+function TagsHeader() {
   return (
     <div className="text-gray-500 flex w-full justify-between pb-2 pr-2">
-      <p>Etiketler</p>
-      {showMoreTags && (
-        <button onClick={handleClick} className="font-bold text-accent ">
-          Gizle
-        </button>
-      )}
+      <p>Sık Kullanılanlar</p>
     </div>
-  );
-}
-
-function ShowMoreButton({ showMoreTags, setShowMoreTags }) {
-  const handleClick = () => {
-    setShowMoreTags(true);
-  };
-
-  return (
-    !showMoreTags && (
-      <button
-        className="bottom-2 right-0 left-0 m-auto w-6 h-6 
-    flex items-center justify-center bg-white
-     absolute rounded-full"
-        onClick={handleClick}
-      >
-        <img
-          className="w-4 aspect-square"
-          src="./images/down-arrow.png"
-          alt="down arrow"
-        />
-      </button>
-    )
   );
 }
 
@@ -75,7 +43,7 @@ function Tag({ text, tag, tags, setTags }) {
     <button
       className={`${
         selected ? "bg-accent" : "bg-black"
-      } py-1 px-3 rounded-sm font-semibold transition-all ease-in-out  text-neutral`}
+      } py-1 px-3 whitespace-nowrap rounded-sm font-semibold transition-all ease-in-out  text-neutral`}
       onClick={handleClick}
     >
       {text}
@@ -85,7 +53,7 @@ function Tag({ text, tag, tags, setTags }) {
 
 function TagsContent({ getTags, setTags, tags }) {
   return (
-    <div className="flex flex-wrap gap-3 ">
+    <div className="flex gap-3">
       {tags.map((tag) => {
         return (
           <Tag
@@ -102,51 +70,10 @@ function TagsContent({ getTags, setTags, tags }) {
   );
 }
 
-// bunu pek sevmiyorum değiştirebiliriz
-function ShowMoreGradient({ showMoreTags }) {
+function TagsContentContainer({ getTags, setTags, tags }) {
   return (
-    !showMoreTags && (
-      <div className="bg-gradient-to-t opacity-30 rounded-sm from-black absolute w-full h-full bottom-0"></div>
-    )
-  );
-}
-
-function TagsContentContainer({
-  showMoreTags,
-  setShowMoreTags,
-  getTags,
-  setTags,
-  tags,
-}) {
-  // bu hiç düzgün çalışmıyor değiştirmemiz lazım
-  const lengthOfTags = Math.round(tags.length / 3);
-
-  // Transition eklemek için grid kullanıyoruz
-  // Default olarak 8rem girdik sonraki
-  // Value yu hesaplamak için tags'in kaç tane elementi olduğuna
-  // bakıyoruz
-  // * 3 bi satırda ortalama 3 tane tag olmasından geliyor
-  // ama daha kesin bi sonuç için tag'lerin içindeki toplam harf
-  // sayısıyla bi hesaplama yapabiliriz.
-  const styles = {
-    gridTemplateRows: `${lengthOfTags * 3}rem`,
-  };
-
-  return (
-    <div
-      className="grid transition-all duration-500"
-      style={showMoreTags ? styles : { gridTemplateRows: "6rem" }}
-    >
-      <div className="overflow-y-hidden relative">
-        <>
-          <ShowMoreGradient showMoreTags={showMoreTags} />
-          <ShowMoreButton
-            showMoreTags={showMoreTags}
-            setShowMoreTags={setShowMoreTags}
-          />
-          <TagsContent getTags={getTags} setTags={setTags} tags={tags} />
-        </>
-      </div>
+    <div className="overflow-x-auto relative">
+      <TagsContent getTags={getTags} setTags={setTags} tags={tags} />
     </div>
   );
 }
@@ -167,6 +94,22 @@ function Tags({ getTags }) {
       text: "Maltepe",
       selected: false,
     },
+    {
+      text: "Kadıköy",
+      selected: false,
+    },
+    {
+      text: "Teknoloji",
+      selected: false,
+    },
+    {
+      text: "Mimarlık",
+      selected: false,
+    },
+    {
+      text: "Çok uzun bir tag ismi",
+      selected: false,
+    },
   ]);
 
   useEffect(() => {
@@ -174,22 +117,10 @@ function Tags({ getTags }) {
     getTags(tags);
   }, [tags]);
 
-  // Tags kısmını kontrol ediyor
-  const [showMoreTags, setShowMoreTags] = useState(false);
-
   return (
     <div className="px-3  pt-3">
-      <TagsHeader
-        showMoreTags={showMoreTags}
-        setShowMoreTags={setShowMoreTags}
-      />
-      <TagsContentContainer
-        tags={tags}
-        getTags={getTags}
-        setTags={setTags}
-        showMoreTags={showMoreTags}
-        setShowMoreTags={setShowMoreTags}
-      />
+      <TagsHeader />
+      <TagsContentContainer tags={tags} getTags={getTags} setTags={setTags} />
     </div>
   );
 }
