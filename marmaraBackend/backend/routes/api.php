7f22bc28f003881e3b9a4afdf2bb82987dfc80f6
muratcankargi <muratcankargi@hotmail.com>
2023-10-13 14:41:12 +0300

    update api.php

--- marmaraBackend/backend/routes/api.php
+++ marmaraBackend/backend/routes/api.php
@@ -21,18 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:api')->group(function () {
+
+    Route::post('/declarations', [DeclarationController::class, 'createDeclaration']);
+    Route::get('/declarations', [DeclarationController::class, 'getDeclarations']);
+    Route::get('/declarations/{id}', [DeclarationController::class, 'getDeclaration']);
+    Route::put('/declarations/{id}', [DeclarationController::class, 'updateDeclaration']);
+    Route::delete('declarations/{id}', [DeclarationController::class, 'deleteDeclaration']);
+
+    Route::post('/tags', [TagController::class, 'createTag']);
+    Route::get('/tags', [TagController::class, 'getTags']);
+    Route::put('/tags/{id}', [TagController::class, 'updateTag']);
+    Route::delete('tags/{id}', [TagController::class, 'deleteTag']);
+});
+
 Route::post('/isStudent', [UserController::class, 'isStudent']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/saveUser', [UserController::class, 'saveUser']);
 Route::post('/authenticate', [UserController::class, 'authenticate']);
-
-Route::post('/declarations', [DeclarationController::class, 'createDeclaration']);
-Route::get('/declarations', [DeclarationController::class, 'getDeclarations']);
-Route::get('/declarations/{id}', [DeclarationController::class, 'getDeclaration']);
-Route::put('/declarations/{id}', [DeclarationController::class, 'updateDeclaration']);
-Route::delete('declarations/{id}', [DeclarationController::class, 'deleteDeclaration']);
-
-Route::post('/tags', [TagController::class, 'createTag']);
-Route::get('/tags', [TagController::class, 'getTags']);
-Route::put('/tags/{id}', [TagController::class, 'updateTag']);
-Route::delete('tags/{id}', [TagController::class, 'deleteTag']);
