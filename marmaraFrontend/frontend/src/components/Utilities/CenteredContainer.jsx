3d0ab28de810f5fb762a3030eb77ab51c19d6d05
serkan-bayram <serkanbay02@hotmail.com>
2023-11-30 23:24:12 +0300

    update create profile page

--- marmaraFrontend/frontend/src/components/Utilities/CenteredContainer.jsx
+++ marmaraFrontend/frontend/src/components/Utilities/CenteredContainer.jsx
@@ -3,8 +3,6 @@ import { useHamburgerMenu } from "../Contexts/HamburgerMenuContext";
 import { useAllFilters } from "../Contexts/AllFilters";
 import disableScroll from "disable-scroll";
 
-// Hem signInPage hem de SignUpPage de kullanıldığı için oluşturdum
-// büyük ihtimal createProfile de de benzer bi şey kullanılacak
 function CenteredContainer({ children, paddingTop = "pt-40" }) {
   // hambrugermenu active ise sağdaki scroll u kaldırıyoruz yoksa
   // kapatma butonu yerinden oynuyor
